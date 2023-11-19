2707de932b5ea54e5f51e840f2b4c067543223a6
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-11-19 00:31:21 -0500

    set -e, removed cached data

--- crawl_all.sh
+++ crawl_all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 # This spider is mainly responsible for crawling the content from snlarchive 
 # pages for Episodes and their Titles (sketches). Also, IMDB ratings of episodes.
 scrapy crawl snlspider
 # This spider is just responsible for crawling Cast items - who was on the cast 
 # of which seasons, when did they start/end, etc.
 scrapy crawl castspider
+
+echo "crawl_all.sh finished successfully"
